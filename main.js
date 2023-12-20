222c68394231d66af76eff9bffcfae55b5d03ede
Fandi <31383432+fandie@users.noreply.github.com>
2023-12-20 14:05:58 +0700

    Update main.js
    Menambah fungsi dari perpindahan nav tab Skill

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var typed = new Typed(".multiple-text", {
     backDelay: 500,
     loop: true,
   });
-
+//Togglechat
   function toggleChatPopup() {
     var chatPopup = document.getElementById("chatPopup");
     chatPopup.style.display = (chatPopup.style.display === "block") ? "none" : "block";
@@ -16,4 +16,12 @@ var typed = new Typed(".multiple-text", {
 // Menangani penutupan popup chat dengan mengklik tombol close
 document.getElementById("closeChat").addEventListener("click", function () {
     document.getElementById("chatPopup").style.display = "none";
+});
+
+// Perpindahaan nav tab skill
+$(document).ready(function(){
+  $('#myTabs a').click(function(e){
+      e.preventDefault();
+      $(this).tab('show');
+  });
 });
